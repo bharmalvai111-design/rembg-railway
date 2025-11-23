97b332dc14e978e0637f22b080fcbb34a3b18866
bharmal vaishnav <rajumakvanvr55@gmail.com>
2025-11-23 15:04:40 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import Response
 from rembg import remove
 import uvicorn
@@ -9,9 +9,8 @@ app = FastAPI()
 @app.post("/remove")
 async def remove_bg(image: UploadFile = File(...)):
     input_data = await image.read()
-    output_data = remove(input_data)
-    return Response(output_data, media_type="image/png")
+    output = remove(input_data)
+    return Response(output, media_type="image/png")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run("server:app", host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
